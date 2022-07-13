c8994c6e20c2daa8ddb02ae4a9999f70bcf54c80
Irina Afonasenko <108224118+IrinaAfonasenko@users.noreply.github.com>
2022-07-13 10:54:52 +0300

    Add files via upload

---
+++ Python/mango.py
@@ -0,0 +1,28 @@
+name = input("What is your name?")
+print("Hello" + name)
+name_husband = input ("And what is your husband name?")
+print ("I like his name, it is translated from greek as victorious")
+
+#first_name = "Tom"
+#print(first_name)
+#bool = True, False #camelcase
+
+year = input("When were you born?")
+print(year)
+year = input("When was your husband born?")
+print(year)
+
+my_last_name = input("Irina what is your surname?")
+husband_last_name = input("And What is your husband surname?")
+print("Cool nice to meet you" + name_husband + husband_last_name)
+print ("Nice to meet you" + name + my_last_name)
+
+
+year = input("Please put your birth year here: ")
+age = 2022 - int(year)
+print("Your age is:" + str(age))
+
+
+
+
+
